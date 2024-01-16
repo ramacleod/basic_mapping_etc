68eaa438ceab18b4b8fde8417977af4921ce6d79
ramacleod <143741191+ramacleod@users.noreply.github.com>
2024-01-16 11:37:57 +0000

    Create markdup.sh

---
+++ markdup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --partition=medium
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=12G
+#SBATCH --array=0-7
+
+BAM=(*.merged.bam)
+out=${BAM[$SLURM_ARRAY_TASK_ID]//merged.bam/merged.md.bam}
+metrics=${BAM[$SLURM_ARRAY_TASK_ID]//merged.bam/.md.metrics}
+
+conda init bash
+source ~/.bashrc
+conda activate /home/rmacleod/scratch/apps/mambaforge/envs/map-etc
+
+picard MarkDuplicates OPTICAL_DUPLICATE_PIXEL_DISTANCE=12000 I=$BAM o=$out REMOVE_DUPLICATES=false METRICS_FILE=$metrics TAGGING_POLICY=All VALIDATION_STRATEG
+Y=LENIENT
