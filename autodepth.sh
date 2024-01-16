a14ede0b391f06aeceaaf1195adbf9a8c61f0a4c
ramacleod <143741191+ramacleod@users.noreply.github.com>
2024-01-16 15:18:04 +0000

    Update autodepth.sh

--- autodepth.sh
+++ autodepth.sh
@@ -5,11 +5,10 @@
 #SBATCH --mem=5G
 #SBATCH --array=0-11
 
-conda init bash
-source ~/.bashrc
 FILES=(*.bam)
 
+conda init bash
+source ~/.bashrc
 conda activate /home/rmacleod/scratch/apps/mambaforge/envs/map-etc
 
-samtools depth -a -Q 30 -q 20 -b /home/rmacleod/scratch/private/shamanka2/hg38/WholeGenomeFasta/genome.bed ${FILES[$SLURM_ARRAY_TASK_ID]} | datamash mean 3 >
-${FILES[$SLURM_ARRAY_TASK_ID]}.autodepth.txt
+samtools depth -a -Q 30 -q 20 -b /home/rmacleod/scratch/private/shamanka2/hg38/WholeGenomeFasta/genome.bed ${FILES[$SLURM_ARRAY_TASK_ID]} | datamash mean 3 > ${FILES[$SLURM_ARRAY_TASK_ID]}.autodepth.txt
