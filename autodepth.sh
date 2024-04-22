5e4da3f803120a5304e9f808e29e466c5dc1f817
ramacleod <143741191+ramacleod@users.noreply.github.com>
2024-04-22 19:31:12 +0100

    add 1240K positions

--- autodepth.sh
+++ autodepth.sh
@@ -12,3 +12,6 @@ source ~/.bashrc
 conda activate /home/rmacleod/scratch/apps/mambaforge/envs/map-etc
 
 samtools depth -a -Q 30 -q 20 -b /home/rmacleod/scratch/private/shamanka2/hg38/WholeGenomeFasta/genome.bed ${FILES[$SLURM_ARRAY_TASK_ID]} | datamash mean 3 > ${FILES[$SLURM_ARRAY_TASK_ID]}.autodepth.txt
+
+#option for 1240K positions
+### samtools depth -a -Q 30 -q 20 -b /mnt/shared/scratch/rmacleod/private/shamanka2/EBA_hg19/bams/1240K_positions.bed ${FILES[$SLURM_ARRAY_TASK_ID]} | datamash mean 3 > ${FILES[$SLURM_ARRAY_TASK_ID]}.autodepth.txt
