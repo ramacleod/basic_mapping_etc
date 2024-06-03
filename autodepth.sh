3e4c1e075acf01ae911502f41dc9ec49f184e4c3
ramacleod <143741191+ramacleod@users.noreply.github.com>
2024-06-03 19:24:59 +0100

    Update autodepth.sh

--- autodepth.sh
+++ autodepth.sh
@@ -15,3 +15,5 @@ samtools depth -a -Q 30 -q 20 -b /home/rmacleod/scratch/private/shamanka2/hg38/W
 
 #option for 1240K positions
 ### samtools depth -Q 30 -q 20 -b /mnt/shared/scratch/rmacleod/private/shamanka2/EBA_hg19/bams/1240K_positions.bed ${FILES[$SLURM_ARRAY_TASK_ID]} | datamash mean 3 > ${FILES[$SLURM_ARRAY_TASK_ID]}.autodepth.txt
+#count SNPs with >= 1 read coverage
+### for i in *.bam ; do echo $i ; samtools depth -Q 30 -q 20 -b chr1-22_1240K_positions.bed $i | awk '$3!="0"'| wc -l ; done
